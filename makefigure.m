d5ff0a6871d35463917b691dbb8b9160f8c6309b
edwardtekwa <wong.tek.wa@gmail.com>
2021-12-20 20:31:31 -0500

    Delete makefigure.m

--- makefigure.m
+++
@@ -1,5 +0,0 @@
-plot(randn(100,1)); 
-set(gca,'Units','normalized','Position',[0.13 0.11 0.775 0.815]);
-set(gcf,'Units','pixels','Position',[4 4 1200 900]);  %# Modify figure size
-hgexport(gcf,'myfig.png',...
-    hgexport('factorystyle'),'Format','png');
