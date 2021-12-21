6ecebfc3ae1d1048dfad817514dcb097d0aa1f6c
edwardtekwa <wong.tek.wa@gmail.com>
2021-12-20 20:38:13 -0500

    Delete stats_param_c.m

--- stats_param_c.m
+++
@@ -1,5 +0,0 @@
-
-BiomassSS=sum((totBiomass(1:12,1)-totBiomass(1:12,3)).^2)
-ProdSS=sum((totProd(1:12,1)-totProd(1:12,3)).^2)
-BiomassMeanDiff=100*mean((totBiomass(1:12,3)-totBiomass(1:12,1))./totBiomass(1:12,1))
-ProdMeanDiff=100*mean((totProd(1:12,3)-totProd(1:12,1))./totProd(1:12,1))
